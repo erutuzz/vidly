cf95b997393a0142ccee1e8a3fb2c70f52a2ec0f
V <1553221481@qq.com>
2022-01-01 13:17:27 +0800

    adding register and logout

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "../common/form";
+// import * as loginService from "../services/authService";
+import auth from "../services/authService";
 
 class LoginForm extends Form {
   //   username = React.createRef();
@@ -18,9 +20,23 @@ class LoginForm extends Form {
   //     this.username.current.focus();
   //   }
 
-  doSubmit = () => {
+  doSubmit = async () => {
     // call the server
-    console.log("submitted");
+    // console.log("submitted");
+    try {
+      const { data } = this.state;
+      // const a = await login(data.username, data.password);
+      // console.log(a);
+      await auth.login(data.username, data.password);
+      // this.props.history.push("/");
+      window.location = "/";
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "../common/form";
+import { register } from "../services/userService";
+import auth from "../services/authService";
 
 class RegisterForm extends Form {
   state = {
@@ -14,9 +16,21 @@ class RegisterForm extends Form {
     name: Joi.string().required().label("Name"),
   };
 
-  doSubmit = () => {
+  doSubmit = async () => {
     // call the server
-    console.log("registered");
+    // console.log("registered");
+    try {
+      const response = await register(this.state.data);
+      auth.loginWithJwt(response.headers["x-auth-token"]);
+      // this.props.history.push("/");
+      window.location = "/";
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
